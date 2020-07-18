702cd124be606dc7b173686e693af0d6861553b7
Zac Evans <zacevanscoding@gmail.com>
2020-07-18 12:08:02 -0400

    background

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+    
+    // $.get('https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breeds/image/random/').done
+    //     (function (data) {
+    //         $('#randomDogCarouselOne').html(renderDogImage(data.message))
+    //         })
+    //         .fail(function (error) {
+    //         console.log(error);
+    //         });
+        
+    // setInterval (function() {
+    //     $.get('https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breeds/image/random/').done
+    //         (function (data) {
+    //             $('#randomDogCarouselOne').html(renderDogImage(data.message))
+    //             $('#randomDogCarouselTwo').html(renderDogImage(data.message));
+    //             })
+    //             .fail(function (error) {
+    //             console.log(error);
+    //             })
+    //     }, 4000);
+
     $('#generateRandomDog').click(function() {
         console.log('button clicked')
         $.get('https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breeds/image/random/').done
@@ -11,31 +31,39 @@ $(document).ready(function() {
             })
         });
         
-
+    
+   
     $('#submitButton').click(function(event) {
         
         event.preventDefault();
         console.log('submit button clicked')
 
+        // var params = $(this).serializeArray();
+
+        // console.log(params);
+        
         var favoriteDog = document.getElementById('favoriteDogFormInput').value;
+
         var favoriteDog = favoriteDog.toLowerCase();
-        console.log(favoriteDog)
+        console.log(favoriteDog);
 
         
-        $.get(`https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breed/${favoriteDog}/images/random`).done
-            (function (data) {
-                console.log(data);
-                $('#randomDogImage').html(renderDogImage(data.message));
-                $('#submitButton').text('Ta-da! There is your doggo. Click again for more.')
-            })
-            .fail(function (error) {
-                console.log(error);
-                $('#submitButton').text('Sorry! That aint no doggo. Try again.');
-                $('#randomDogImage').html('');
-
-            });  
-        });
+        
+    $.get(`https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breed/${favoriteDog}/images/random`).done
+        (function (data) {
+            console.log(data);
+            $('#randomDogImage').html(renderDogImage(data.message));
+            $('#submitButton').text('Ta-da! There is your doggo. Click again for more.')
+        })
+        .fail(function (error) {
+            console.log(error);
+            $('#submitButton').text('Sorry! That aint no doggo. Try again.');
+            $('#randomDogImage').html('');
+        });  
     });
+});
+
+   
 
 
 function renderDogImage(image){
